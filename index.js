a71b80c8651a9e82c45361739124a642e0883e24
stanselldev <stanselldev@gmail.com>
2019-12-23 16:11:30 -0500

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 
 import Router from "./Components/Router/Router";
 
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <Router />
-      </div>
-    );
-  }
+const Index = () => {
+  
+
+  return (
+    <div>
+      <Router />
+    </div>
+  )
 }
 
 ReactDOM.render(<Index />, document.getElementById("root"));
